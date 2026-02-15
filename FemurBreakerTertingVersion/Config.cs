c43b04753689297f8433f5d8a2b40d95825a5a8a
davilone33 <126024362+davilone33@users.noreply.github.com>
2026-02-15 21:27:46 +0100

    Update Config.cs

--- FemurBreakerTertingVersion/Config.cs
+++ FemurBreakerTertingVersion/Config.cs
@@ -12,12 +12,6 @@ namespace FemurBreakerTertingVersion
         public string CassieAnounceWhitPlayerDead = "A player sacrificed himself for the recontainment of scp 106";
         [Description("OnRecontainmentDeath")]
         public string OnRecontainmentDeath { get; set; } = "recontained!";
-        [Description("Bot name")]
-        public string OnNameBot { get; set; } = "C.A.S.S.I.E";
-        [Description("If you want the femurbreaker sound activate this")]
-        public bool npc { get; set; } = true;
-        [Description("seconds of audio")]
-        public float seconds { get; set; } = 33f;
         [Description("Activate this if you want a cassie to say when someone sacrifices")]
         public bool CassieWithSacrificie { get; set; } = true;
         [Description("UseCassie")]
@@ -28,14 +22,6 @@ namespace FemurBreakerTertingVersion
         public int Generators { get; set; } = 3;
         [Description("How many generators so that it can be activated")]
         public string TextGenerators { get; set; } = "Generators: ";
-        [Description("The bot id")]
-        public int BotId = 99;
-        [Description("The bot badget name")]
-        public string BotBadgetName = "C.A.S.S.I.E";
-        [Description("The bot id")]
-        public string BadgetBotColor = "orange";
-        [Description("The file name (The audio must be in ogg format.)")]
-        public string FileAudioName = "FemurSound";
 
         [Description("Onfailure")]
         public string OnFailure { get; set; } = "¡The recontainment of scp 106 has failed! Find another person for recontainment!";
